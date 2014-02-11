9cf8cdb0021e3daa15bde73bb08bb45db6d28a08
Simon Norton <simon.norton@gmail.com>
2014-02-11 19:30:16 +0000

    fixed form population for test screen also

--- FORMS/test-form.php
+++ FORMS/test-form.php
@@ -4,15 +4,16 @@ function display_test_form($row,$createOrUpdate,$template){
 if($createOrUpdate == "updatePage.php"){
 	$update = "update page";
 }else{
+	$new = TRUE;
 	$update = "create page";
 }
 ?>
 <p>THIS IS A test FORM</p>
 <form name="input" action="<?php echo $createOrUpdate ?>" method="POST">
 title:<br />
-<textarea name="title" rows="1" cols="40"><?php echo $row['title']; ?></textarea><br />
+<textarea name="title" rows="1" cols="40"><?php if(!$new) echo $row['title']; ?></textarea><br />
 content:<br/>
-<textarea name="title" rows="1" cols="40"><?php echo $row['content']; ?></textarea><br />
+<textarea name="title" rows="1" cols="40"><?php if(!$new) echo $row['content']; ?></textarea><br />
 <input type="hidden" name="template" value="<?php echo $template ?>" />
 <input type="submit" value="<?php echo $update ?>">
 </form>
