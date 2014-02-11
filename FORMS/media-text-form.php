cbdb4e4218452f3615f6f4ce585318b0f1af6714
Simon Norton <simon.norton@gmail.com>
2014-02-11 19:28:50 +0000

    new form not populated

--- FORMS/media-text-form.php
+++ FORMS/media-text-form.php
@@ -1,9 +1,10 @@
 <?php
 function display_media_text_form($row,$createOrUpdate,$template){
 	if($createOrUpdate == "updatePage.php"){
-		$update = "update page";
+		$button = "update page";
 	}else{
-		$update = "create page";
+		$new = TRUE;
+		$button = "create page";		
 	}
 //DO NOT NEED TO PULL FROM DB IF IT'S NEW!
 ?>
@@ -13,15 +14,15 @@ function display_media_text_form($row,$createOrUpdate,$template){
 <form name="input" action="<?php echo $createOrUpdate ?>" method="POST">
 
 title:<br />
-<textarea name="title" rows="1" cols="40"><?php echo $row['title']; ?></textarea><br />
+<textarea name="title" rows="1" cols="40"><?php if(!$new) echo $row['title']; ?></textarea><br />
 content:<br />
-<textarea name="content" rows="10" cols="60"><?php echo $row['content']; ?></textarea><br />
+<textarea name="content" rows="10" cols="60"><?php if(!$new) echo $row['content']; ?></textarea><br />
 Link:<br />
-<textarea name="link" rows="1" cols="20"><?php echo $row['link']; ?></textarea><br />
+<textarea name="link" rows="1" cols="20"><?php if(!$new) echo $row['link']; ?></textarea><br />
 subtitles:</br>
-<textarea name="cc" rows="10" cols="60"><?php echo $row['cc']; ?></textarea><br />
+<textarea name="cc" rows="10" cols="60"><?php if(!$new) echo $row['cc']; ?></textarea><br />
 <input type="hidden" name="template" value="<?php echo $template ?>" />
-<input type="submit" value="<?php echo $update ?>">
+<input type="submit" value="<?php echo $button ?>">
 </form>
 <?php
 }
